9603e793d218bba7a4024fa5074f610b6ab5852a
dage23 <46784507+dage23@users.noreply.github.com>
2019-08-26 17:39:05 -0600

    Creada lista de caracteres y array con frecuencia

--- Lab1-ED2/Controllers/HuffmanController.cs
+++ Lab1-ED2/Controllers/HuffmanController.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.IO;
-
+using Lab1_ED2.Models;
 namespace Lab1_ED2.Controllers
 {
     public class HuffmanController : Controller
     {
+        
         public ActionResult Importar()
         {
             return View();
@@ -27,7 +28,34 @@ namespace Lab1_ED2.Controllers
                 Rutaarchivo = Ruta + Path.GetFileName(ArchivoImportado.FileName);
                 string extension = Path.GetExtension(ArchivoImportado.FileName);
                 ArchivoImportado.SaveAs(Rutaarchivo);
-                string csvData = System.IO.File.ReadAllText(Rutaarchivo);
+                string TextoArchivo = System.IO.File.ReadAllText(Rutaarchivo);
+                char[] ArregloTexto = TextoArchivo.ToCharArray();
+                var ListaCaracteres = new List<char>();
+                for (int i = 0; i < ArregloTexto.Length; i++)
+                {
+                    if (!(ListaCaracteres.Contains(ArregloTexto[i])))
+                    {
+                        ListaCaracteres.Add(ArregloTexto[i]);
+                    }
+                }
+                var FrecuenciaCaracteres = new int[ListaCaracteres.Count];
+                for (int i = 0; i < FrecuenciaCaracteres.Length; i++)
+                {
+                    FrecuenciaCaracteres[i] = 0;
+                }
+
+                for (int i = 0; i < ListaCaracteres.Count; i++)
+                {
+                    char CaracterEvaluando = ListaCaracteres[i];
+                    for (int j = 0; j < ArregloTexto.Length; j++)
+                    {
+                        if (CaracterEvaluando==ArregloTexto[j])
+                        {
+                            FrecuenciaCaracteres[i]++;
+                        }
+                    }
+                }
+
             }
                 return View();
         }
---
+++ Lab1-ED2/Models/Caracter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab1_ED2.Models
+{
+    public class Caracter:IComparable
+    {
+        public char CaracterTexto { get; set; }
+        public int Frecuencia { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            var vComparador = (Caracter)obj;
+            return CaracterTexto.CompareTo(vComparador.CaracterTexto);
+        }
+    }
+}
